4781e65b943fcbdcf6d0f9032e732d2ed6524d3d
thomas lombardo <tomlom1984@hotmail.co.uk>
2019-06-29 10:55:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,11 +7,11 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls)
-calls.any? {|x| x.length > 4}
+calls.any? {|i| i.length > 4}
 end
 
 def find_the_cheese(cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-cheese.find {|type| cheese_types.include?(type)}
+cheese.find {|i| cheese_types.include?(i)}
 end
